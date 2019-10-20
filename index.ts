4c9c3aaecd08474c6f23e085c43282a68925cc01
JohnJasonFisher <jjfisher91@gmail.com>
2019-10-20 15:53:36 -0500

    WE MADE A HAND!!!

--- index.ts
+++ index.ts
@@ -1,10 +1,93 @@
 import {convertFileToDeckList} from './utils/TextFileConverter'
 import {toPlayDeck} from './utils/toPlayDeck'
 
+import PlayingCard from './domain/PlayingCard'
 
 console.log('Start Script!')
 
-const deckList = convertFileToDeckList('./decklist.txt')
-const deck = toPlayDeck(deckList)
+// const deckList = convertFileToDeckList('./decklist.txt')
+// const deck = toPlayDeck(deckList)
 
-console.log(deck)
+// setTimeout(function(){ console.log(deck) }, 60000)
+
+let deck = [
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Mountain', cost: 0, type: 'land' },
+    { name: 'Lava Spike', cost: 1, type: 'Sorcery' },
+    { name: 'Lava Spike', cost: 1, type: 'Sorcery' },
+    { name: 'Lava Spike', cost: 1, type: 'Sorcery' },
+    { name: 'Lava Spike', cost: 1, type: 'Sorcery' },
+    { name: 'Lightning Bolt', cost: 1, type: 'Instant' },
+    { name: 'Lightning Bolt', cost: 1, type: 'Instant' },
+    { name: 'Lightning Bolt', cost: 1, type: 'Instant' },
+    { name: 'Lightning Bolt', cost: 1, type: 'Instant' },
+    { name: 'Shard Volley', cost: 1, type: 'Instant' },
+    { name: 'Shard Volley', cost: 1, type: 'Instant' },
+    { name: 'Shard Volley', cost: 1, type: 'Instant' },
+    { name: 'Rift Bolt', cost: 3, type: 'Sorcery' },
+    { name: 'Rift Bolt', cost: 3, type: 'Sorcery' },
+    { name: 'Rift Bolt', cost: 3, type: 'Sorcery' },
+    { name: 'Rift Bolt', cost: 3, type: 'Sorcery' },
+    { name: 'Galvanic Blast', cost: 1, type: 'Instant' },
+    { name: 'Galvanic Blast', cost: 1, type: 'Instant' },
+    { name: 'Galvanic Blast', cost: 1, type: 'Instant' },
+    { name: 'Galvanic Blast', cost: 1, type: 'Instant' },
+    { name: 'Light Up the Stage', cost: 3, type: 'Sorcery' },
+    { name: 'Light Up the Stage', cost: 3, type: 'Sorcery' },
+    { name: 'Seal of Fire', cost: 1, type: 'Enchantment' },
+    { name: 'Seal of Fire', cost: 1, type: 'Enchantment' },
+    { name: 'Seal of Fire', cost: 1, type: 'Enchantment' },
+    { name: 'Seal of Fire', cost: 1, type: 'Enchantment' },
+    { name: "Wizard's Lightning", cost: 3, type: 'Instant' },
+    { name: "Wizard's Lightning", cost: 3, type: 'Instant' },
+    { name: "Wizard's Lightning", cost: 3, type: 'Instant' },
+    { name: "Wizard's Lightning", cost: 3, type: 'Instant' },
+    { name: 'Skewer the Critics', cost: 3, type: 'Sorcery' },
+    { name: 'Skewer the Critics', cost: 3, type: 'Sorcery' },
+    { name: 'Skewer the Critics', cost: 3, type: 'Sorcery' },
+    { name: 'Skewer the Critics', cost: 3, type: 'Sorcery' },
+    { name: 'Slaying Fire', cost: 3, type: 'Instant' },
+    { name: 'Slaying Fire', cost: 3, type: 'Instant' },
+    { name: 'Slaying Fire', cost: 3, type: 'Instant' },
+    { name: 'Slaying Fire', cost: 3, type: 'Instant' },
+    { name: 'Shock', cost: 1, type: 'Instant' },
+    { name: 'Shock', cost: 1, type: 'Instant' },
+    { name: 'Shock', cost: 1, type: 'Instant' },
+    { name: 'Shock', cost: 1, type: 'Instant' }
+  ]
+
+  const playingDeck = deck.map(item =>
+    new PlayingCard(item.name, item.cost, item.type)
+)
+
+// Start the game
+
+const EnemyLife = 20
+// shuffle(playingDeck)
+
+const hand = []
+
+for (var i = 0; i < 7; i++) {
+    hand.push(playingDeck.pop())
+}
+
+console.log(hand.length)
+console.log(hand)
+console.log(playingDeck.length)
--- utils/toPlayDeck.ts
+++ utils/toPlayDeck.ts
@@ -1,7 +1,9 @@
 import DeckCard from '../domain/DeckCard'
 import PlayingCard from '../domain/PlayingCard'
 
-export const toPlayDeck = (decklist: DeckCard[]) => {
+import {Cards} from 'mtgsdk-ts'
+
+export const toPlayDeck = (decklist: DeckCard[]): PlayingCard[] => {
     const newPlayDeck: PlayingCard[]= []
     decklist.forEach(item => {
         const name = item.name
@@ -9,16 +11,35 @@ export const toPlayDeck = (decklist: DeckCard[]) => {
         let cost = 0
         let type = 'land'
 
-        const newPlayCard = new PlayingCard(name, cost, type)
-
         if (name !== 'Mountain') {
+            let carddata = getCardData(name)
+            .then(res => {
+                cost = res.cmc
+                type = res.type
 
-        }
+                const newPlayCard = new PlayingCard(name, cost, type)
 
-        for (var i = 0; i < quantity; i++) {
-            newPlayDeck.push(newPlayCard)
+                for (var i = 0; i < quantity; i++) {
+                    newPlayDeck.push(newPlayCard)
+                }
+                return newPlayDeck
+            })
+        } else {
+            const newPlayCard = new PlayingCard(name, cost, type)
+
+            for (var i = 0; i < quantity; i++) {
+                newPlayDeck.push(newPlayCard)
+            }
         }
     })
 
     return newPlayDeck
+}
+
+const getCardData = (name: string) => {
+    return Cards.where({name: name}).then(results => {
+        var result = {cmc: results[0].cmc, type: results[0].types[0]}
+        // console.log(result)
+        return result
+    })
 }
