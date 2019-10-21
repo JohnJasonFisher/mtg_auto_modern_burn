c8737968faec6d44f4bf37eb73accf1204efebb7
JohnJasonFisher <jjfisher91@gmail.com>
2019-10-20 21:08:02 -0500

    Muligans on 1 one cmc spell

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 
 import {shuffleDeck} from './gameMechanics/shuffleDeck'
 import PlayingCard from './domain/PlayingCard'
-import * as playdeckJson from "./decks.json"
-import { draw } from './gameMechanics/draw'
+import playdeckJson from './decks.json'
+import {draw} from './gameMechanics/draw'
 
 console.log('Start Script!')
 console.log('Using the burn deck')
+
+console.log(playdeckJson["burn"])
 
 const playingDeck: PlayingCard[] = playdeckJson['burn'].map( (item: any) =>
     new PlayingCard(item.name, item.cost, item.type)
@@ -17,10 +19,10 @@ const EnemyLife = 20
 
 shuffleDeck(playingDeck)
 
-const hand: PlayingCard[] = []
+const hand: any[] = []
 
 for (var i = 0; i < 7; i++) {
-    draw(playingDeck)
+    draw(playingDeck, hand)
 }
 
 console.log('*** Starting hand ***')
@@ -28,18 +30,33 @@ console.log(hand.length)
 console.log(hand)
 
 const keepHand = (hand: PlayingCard[]): boolean => {
-    let shouldKeep = false
+    let shouldKeep = true
     let landCount = 0
 
     hand.forEach(card =>
         card.type === 'land' ? landCount += 1 : null
     )
 
-    landCount >= 2 && landCount <= 3 ? shouldKeep = true : null
-    
-    // TODO: look for 1 spell with cmc < 1
+    if (landCount === 1 || landCount > 3) {
+        shouldKeep = false
+    }
+
+    if (!hasCmcOneSpell(hand)) {
+        shouldKeep = false
+    }
 
     return shouldKeep
+}
+
+const hasCmcOneSpell = (hand: PlayingCard[]): boolean => {
+    const listOfCmcSpells = hand.filter((card: PlayingCard) => card.type != 'land' && card.cost == 1)
+    console.log('list of 1 cmc spells')
+    console.log(listOfCmcSpells)
+    if (listOfCmcSpells.length < 1) {
+        return false
+    }
+
+    return true
 }
 
 console.log(keepHand(hand))
